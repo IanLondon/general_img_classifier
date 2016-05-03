5ca51471d57467061ffc9c8938527f79cd8a7b7f
IanLondon <ianlondondu@gmail.com>
2016-05-02 21:32:00 -0400

    Working panda classifier (trained on tiny dataset) and working google image ripper.

---
+++ selenium_google_img_rip.py
@@ -0,0 +1,105 @@
+# Ian London 2016
+# couldn't get anything I found to work... do it myself
+# this downloads thumbnails from Google Images.
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import base64
+import re
+import glob
+import urllib
+
+QUERY = 'panda'
+DOWNLOAD_DIR = '%s_rip' % QUERY
+
+target_url_str = "https://www.google.com/search?as_st=y&tbm=isch&hl=en&as_q=%s&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:m" % QUERY
+
+image_xpath = "//img[@class='rg_i']"
+
+driver = webdriver.Firefox()
+driver.get(target_url_str)
+
+# function to handle dynamic page content loading - using Selenium
+# modified from http://sqa.stackexchange.com/questions/3499/how-to-scroll-to-bottom-of-page-in-selenium-ide
+# (thanks Polyakoff)
+def scroll_down():
+    # define initial page height for 'while' loop
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        time.sleep(8)
+        try:
+            driver.find_element_by_id('smb').click()
+        except:
+            print 'no See More button found...'
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+            break
+        else:
+            last_height = new_height
+
+
+def hover(el):
+    ActionChains(driver).move_to_element(el).perform()
+
+def right_click_save_as(el):
+    ActionChains(driver).move_to_element(el) \
+    .context_click(el) \
+    .send_keys('V') \
+    .perform()
+
+def save_img_src(el, file_no, sleep_time=0.25):
+    base = el.get_attribute('src')
+    # just guess jpeg, probably no file ext in url...
+    file_name_full = '%s/%s.%s' % (DOWNLOAD_DIR, file_no, 'JPEG')
+    urllib.urlretrieve(base, file_name_full)
+    print 'wrote from url %s' % file_name_full
+    time.sleep(sleep_time)
+
+# Google image thumbnails are base64 html strings...
+def dl_base64_img(el, file_no, sleep_time=0.25):
+    hover(el)
+    time.sleep(0.25)
+
+    base = el.get_attribute('src')
+    if not base:
+        print 'no img', file_no
+        return
+
+    base_clean = base[base.find(','):]
+    try:
+        base_filetype = re.findall(r'image/(.*);', base)[0]
+    except IndexError:
+        print 'no img filetype... trying to save src', file_no
+        save_img_src(el, file_no)
+        return
+
+    file_name_full = '%s/%s.%s' % (DOWNLOAD_DIR, file_no, base_filetype)
+    with open(file_name_full, 'w') as f:
+        f.write(base64.decodestring(base_clean))
+
+    print 'wrote %s' % file_name_full
+    time.sleep(sleep_time)
+
+if __name__ == "__main__":
+    # TODO: use command line args instead of hard-coded vars (eg for query)
+
+    # scroll down to load many images
+    scroll_down()
+
+    prev_file_no = 396 #on an aborted scrape, set this to the last file written + 1
+    imgs = driver.find_elements_by_xpath(image_xpath)
+    # iterate thru all images, and when you're done, check to see if there are any more
+    # for some reason the first image_xpath returns only 100 images,
+    # so you have to keep doing find_elements_by_xpath again and again
+    while prev_file_no < 1000 or len(imgs) == prev_file_no:
+        scroll_down()
+        imgs = driver.find_elements_by_xpath(image_xpath)
+        print 'new loop. found %i images, prev_file_no was %i' % (len(imgs), prev_file_no)
+        for file_no, img_el in enumerate(imgs[prev_file_no:]):
+            dl_base64_img(img_el, file_no+prev_file_no)
+        prev_file_no = len(imgs)
---
+++ visual_bow.py
@@ -0,0 +1,144 @@
+# Ian London 2016
+# tools to run a Visual Bag of Words classifier on any images
+
+import cv2
+import numpy as np
+import glob
+import os
+from sklearn.cluster import KMeans
+
+print 'OpenCV VERSION (should be 3.1.0 or later, with nonfree modules installed!):', cv2.__version__
+
+def read_image(path):
+    img = cv2.imread(path)
+    if img is None:
+        raise IOError("Unable to open '%s'. Are you sure it's a valid image path?")
+    return img
+
+def neg_img_cal101(positive_folder, cal101_root='101_ObjectCategories', image_suffix='*.jpg'):
+    """Simply return list of paths for all images in cal101 dataset, except those in positive_folder."""
+    return [path for path in glob.glob(cal101_root + '/*/' + image_suffix) if positive_folder not in path]
+
+def binary_labeled_img_from_cal101(positive_folder, cal101_root='101_ObjectCategories', image_suffix='*.jpg'):
+    """
+    Generate a balanced dataset of positive and negative images from a directory of images
+    where each type of image is separated in its own folder.
+
+    Returns:
+    --------
+    labeled_img_paths: list of lists
+        Of the form [[image_path, label], ...]
+        Where label is True or False for positive and negative images respectively
+    """
+    all_imgs = set(glob.glob(cal101_root + '/*/' + image_suffix))
+    pos_imgs = set(glob.glob(os.path.join(cal101_root, positive_folder) + '/' + image_suffix))
+    neg_imgs = all_imgs - pos_imgs
+
+    neg_sample_size = len(pos_imgs)
+    selected_negs = np.random.choice(list(neg_imgs), size=neg_sample_size, replace=False)
+
+    print '%i positive, %i negative images selected (out of %i negatives total)' % (
+        len(pos_imgs), len(selected_negs), len(neg_imgs))
+
+    labeled_img_paths = [[path, True] for path in pos_imgs] + [[path, False] for path in selected_negs]
+
+    return np.array(labeled_img_paths)
+
+
+def gen_bow_features(labeled_img_paths, test_train_ratio, K_clusters):
+    """
+    Generate "visual bag of words" features for a set of images.
+
+    Parameters:
+    -----------
+    labeled_img_paths: list of lists
+        Of the form [[image_path, label], ...]
+        The label should identify the image type. Eg 'schooner', or 18, (or even True/False for a binary detector)
+        The image_path should the full absolute path name as a string
+
+    test_train_ratio : float
+        Percentage of test rows to training rows.
+
+    K_clusters : int
+        The 'K' in KMeans. This equals the number of final "visual words" you get, which
+        also equals the number of feature columns in your final X matrices.
+
+    Returns:
+    --------
+    X_train, X_test, y_train, y_test, kmeans :
+        X's have K feature columns, each column corresponding to a visual word
+    """
+    if not 0 < test_train_ratio < 1:
+        raise ValueError('test_train_ratio must be between 0 and 1 (non-inclusive)')
+
+    # img_keypoints = {}
+    img_descs = []
+
+    print 'generating SIFT keypoints for %i images' % len(labeled_img_paths)
+
+    for img_path, label in labeled_img_paths:
+        img = read_image(img_path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kp, desc = sift.detectAndCompute(gray, None)
+        # img_keypoints[img_path] = kp
+        img_descs.append(desc)
+
+    # Generate indexes of training rows
+    total_rows = len(img_descs)
+    row_range = range(total_rows)
+    no_training_rows = int(total_rows*(1-test_train_ratio))
+    training_idxs = np.random.choice(row_range, size=no_training_rows, replace=False)
+    test_idxs = np.array(list(set(row_range) - set(training_idxs)))
+
+    print 'Train-test split: %i training rows, %i test rows' % (len(training_idxs), len(test_idxs))
+
+    # Concatenate all descriptors in the training set together
+    training_descs = [img_descs[i] for i in training_idxs]
+    all_descriptors = [desc for desc_list in training_descs for desc in desc_list]
+    all_descriptors = np.array(all_descriptors)
+
+    assert all_descriptors.shape[1] == 128
+    print '%i descriptors before clustering' % all_descriptors.shape[0]
+
+
+    # Cluster descriptors to get codebook
+    print 'Clustering on training set to get codebook of %i words' % K_clusters
+    kmeans = KMeans(n_clusters=K_clusters, n_jobs=-1)
+    # train kmeans on those descriptors selected above
+    kmeans.fit(all_descriptors)
+    print 'done clustering'
+
+    # compute set of cluster-reduced words for each image
+    img_clustered_words = [kmeans.predict(raw_words) for raw_words in img_descs]
+
+    # finally make a histogram of clustered word counts for each image. These are the final features.
+    img_bow_hist = np.array(
+        [np.bincount(clustered_words, minlength=K_clusters) for clustered_words in img_clustered_words])
+
+    X_train = img_bow_hist[training_idxs]
+    X_test = img_bow_hist[test_idxs]
+
+    y_labels = np.array(labeled_img_paths)[:,1]
+    y_train = y_labels[training_idxs]
+    y_test = y_labels[test_idxs]
+
+    return X_train, X_test, y_train, y_test, kmeans
+
+def img_to_vect(img_path, kmeans):
+    """
+    Given an image path and a trained kmeans classifier,
+    generates a feature vector representing that image
+    """
+
+    img = read_image(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, desc = sift.detectAndCompute(gray, None)
+
+    clustered_desc = kmeans.predict(desc)
+    img_bow_hist = np.bincount(clustered_desc, minlength=kmeans.n_clusters)
+
+    # reshape to an array containing 1 array: array[[1,2,3]]
+    # to make sklearn happy (it doesn't like 1d arrays as data!)
+    return img_bow_hist.reshape(1,-1)
